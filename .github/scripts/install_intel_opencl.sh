d53f9d51dd6fd078a18ed48dba00a3736fc9ffc2
AIAirEngineer <pavlovv1993@gmail.com>
2026-09-16 22:37:52 +0300

    change script

--- .github/scripts/install_intel_opencl.sh
+++ .github/scripts/install_intel_opencl.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 # exit script on failure
-set -ev
+set -euo pipefail
+set -v
+
+# ask sudo password
+echo "========================================"
+echo "This script requires sudo privileges."
+echo "Please enter your sudo password below."
+echo "Note: nothing will appear while typing the password."
+echo "========================================"
+
+# Ask for sudo password upfront and cache credentials.
+sudo -v
+
+echo
+echo "sudo authentication successful."
+echo
+
 
 # 1) Add Intel oneAPI APT repo + key  (Ubuntu 24.04 Noble)
 wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
@@ -14,3 +30,6 @@ sudo apt install -yq intel-oneapi-runtime-opencl   # OpenCL runtime (includes IC
 # 3) Verify devices
 sudo apt install -yq clinfo
 clinfo -l  # should show: Platform "Intel(R) CPU Runtime for OpenCL(TM) Applications" and your CPU
+
+# 4) install cmake
+sudo apt install cmake -y
