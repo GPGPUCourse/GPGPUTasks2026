0719e857f78024dbbaf80b046211c1107d43f4bb
Vitaly Egorov <vitaly-egorow21@yandex.ru>
2026-09-18 20:48:16 +0300

    Done

--- src/kernels/cu/aplusb_matrix_good.cu
+++ src/kernels/cu/aplusb_matrix_good.cu
@@ -18,7 +18,9 @@ __global__ void aplusb_matrix_good(const unsigned int* a,
     // т.е. если в матрице сделать шаг вправо или влево на одну ячейку - то в памяти мы шагнем на 4 байта
     // т.е. если в матрице сделать шаг вверх или вниз на одну ячейку - то в памяти мы шагнем на так называемый stride=width*4 байта
 
-    // TODO реализуйте этот кернел - просуммируйте две матрицы так чтобы получить максимально ХОРОШУЮ производительность с точки зрения memory coalesced паттерна доступа
+    const unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+    c[y * width + x] = a[y * width + x] + b[y * width + x];
 }
 
 namespace cuda {
