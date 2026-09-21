a22ddd581e63f0e303e10503cad52a627e9204c1
Stepan Syrtsov <syrtsovss@yandex.ru>
2026-09-21 22:22:42 +0300

    Task01 done

--- src/kernels/cu/aplusb_matrix_bad.cu
+++ src/kernels/cu/aplusb_matrix_bad.cu
@@ -1,3 +1,4 @@
+//#include <__clang_cuda_builtin_vars.h>
 #include <libgpu/context.h>
 #include <libgpu/work_size.h>
 #include <libgpu/shared_device_buffer.h>
@@ -19,6 +20,9 @@ __global__ void aplusb_matrix_bad(const unsigned int* a,
     // т.е. если в матрице сделать шаг вверх или вниз на одну ячейку - то в памяти мы шагнем на так называемый stride=width*4 байта
 
     // TODO реализуйте этот кернел - просуммируйте две матрицы так чтобы получить максимально ПЛОХУЮ производительность с точки зрения memory coalesced паттерна доступа
+    unsigned int x = (threadIdx.x + blockIdx.x * blockDim.x) * 315 % width;
+    unsigned int y = (threadIdx.y + blockIdx.y * blockDim.y) * 251 % height;
+    c[x + y * width] = a[x + y * width] + b[x + y * width];
 }
 
 namespace cuda {
