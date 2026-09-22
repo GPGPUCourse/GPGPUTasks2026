1b2fd1a2a19a0922bc934f1652503272d4e9f138
Ilia Konnov <ilia@sldr.xyz>
2026-09-22 23:01:06 +0300

    task02

--- src/kernels/cu/sum_03_local_memory_atomic_per_workgroup.cu
+++ src/kernels/cu/sum_03_local_memory_atomic_per_workgroup.cu
@@ -11,13 +11,24 @@ __global__ void sum_03_local_memory_atomic_per_workgroup(
     unsigned int* sum,
     unsigned int  n)
 {
-    // Подсказки:
-    // const uint index = blockIdx.x * blockDim.x + threadIdx.x;
-    // const uint local_index = threadIdx.x;
-    // __shared__ unsigned int local_data[GROUP_SIZE];
-    // __syncthreads();
+    const unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int local_index = threadIdx.x;
 
-    // TODO
+    __shared__ unsigned int local_data[GROUP_SIZE];
+
+    local_data[local_index] = (index < n) ? a[index] : 0;
+    __syncthreads();
+
+    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
+        if (local_index < s) {
+            local_data[local_index] += local_data[local_index + s];
+        }
+        __syncthreads();
+    }
+
+    if (local_index == 0) {
+        atomicAdd(sum, local_data[0]);
+    }
 }
 
 namespace cuda {
