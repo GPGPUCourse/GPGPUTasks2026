50ff764cacc85f2026945d79b6f422cbea1e3b56
usernamedoesntmatter <avtimurmain@gmail.com>
2026-09-25 10:16:03 +0300

    v1

--- src/kernels/cu/aplusb_matrix_bad.cu
+++ src/kernels/cu/aplusb_matrix_bad.cu
@@ -1,16 +1,16 @@
 #include <libgpu/context.h>
-#include <libgpu/work_size.h>
 #include <libgpu/shared_device_buffer.h>
+#include <libgpu/work_size.h>
 
 #include <libgpu/cuda/cu/common.cu>
 
 #include "../defines.h"
 
 __global__ void aplusb_matrix_bad(const unsigned int* a,
-                       const unsigned int* b,
-                             unsigned int* c,
-                             unsigned int  width,
-                             unsigned int  height)
+    const unsigned int* b,
+    unsigned int* c,
+    unsigned int width,
+    unsigned int height)
 {
     // все три массива - линейно выложенные двумерные матрицы размера width (число столбиков) x height (число рядов)
     // при этом в памяти подряд идут элементы являющимися соседями в рамках одного ряда,
@@ -18,12 +18,21 @@ __global__ void aplusb_matrix_bad(const unsigned int* a,
     // т.е. если в матрице сделать шаг вправо или влево на одну ячейку - то в памяти мы шагнем на 4 байта
     // т.е. если в матрице сделать шаг вверх или вниз на одну ячейку - то в памяти мы шагнем на так называемый stride=width*4 байта
 
-    // TODO реализуйте этот кернел - просуммируйте две матрицы так чтобы получить максимально ПЛОХУЮ производительность с точки зрения memory coalesced паттерна доступа
+    const unsigned int block_count = gridDim.x * gridDim.y;
+    const unsigned int block_id = blockIdx.x * gridDim.y + blockIdx.y;
+    const unsigned int element_id = threadIdx.x * blockDim.y + threadIdx.y;
+
+    const unsigned int index = block_count * element_id + block_id;
+
+    if (index >= width * height)
+        return;
+
+    c[index] = a[index] + b[index];
 }
 
 namespace cuda {
-void aplusb_matrix_bad(const gpu::WorkSize &workSize,
-            const gpu::gpu_mem_32u &a, const gpu::gpu_mem_32u &b, gpu::gpu_mem_32u &c, unsigned int width, unsigned int height)
+void aplusb_matrix_bad(const gpu::WorkSize& workSize,
+    const gpu::gpu_mem_32u& a, const gpu::gpu_mem_32u& b, gpu::gpu_mem_32u& c, unsigned int width, unsigned int height)
 {
     gpu::Context context;
     rassert(context.type() == gpu::Context::TypeCUDA, 34523543124312, context.type());
--- src/kernels/cu/aplusb_matrix_good.cu
+++ src/kernels/cu/aplusb_matrix_good.cu
@@ -18,7 +18,16 @@ __global__ void aplusb_matrix_good(const unsigned int* a,
     // т.е. если в матрице сделать шаг вправо или влево на одну ячейку - то в памяти мы шагнем на 4 байта
     // т.е. если в матрице сделать шаг вверх или вниз на одну ячейку - то в памяти мы шагнем на так называемый stride=width*4 байта
 
-    // TODO реализуйте этот кернел - просуммируйте две матрицы так чтобы получить максимально ХОРОШУЮ производительность с точки зрения memory coalesced паттерна доступа
+    const unsigned int block_size = blockDim.x * blockDim.y;
+    const unsigned int block_id = blockIdx.y * gridDim.x + blockIdx.x;
+    const unsigned int element_id = threadIdx.y * blockDim.x + threadIdx.x;
+
+    const unsigned int index = block_size * block_id + element_id;
+
+    if (index >= width * height)
+        return;
+
+    c[index] = a[index] + b[index];
 }
 
 namespace cuda {
