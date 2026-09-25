17894250113411bcece4afe6f8260235c5d02298
Mikhail Mikhalovskii <mmmikhalovskii@edu.hse.ru>
2026-09-25 12:11:38 +0300

    fix rassert

--- src/main_aplusb_matrix.cpp
+++ src/main_aplusb_matrix.cpp
@@ -123,7 +123,7 @@ void run(int argc, char** argv)
 
         // Сверяем результат
         for (size_t i = 0; i < width * height; ++i) {
-            rassert(cs[i] == as[i] + bs[i], 321418230421312512, cs[i], as[i] + bs[i], i);
+            rassert(cs[i] == as[i] + bs[i], 321418230365731436, cs[i], as[i] + bs[i], i);
         }
     }
 }
