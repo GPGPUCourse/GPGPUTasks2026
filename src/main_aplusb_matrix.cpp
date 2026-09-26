7efa8bde4d3e1563a6735923f445e3eddfdfa2e0
Pakhomov Semyon <sem.pakhomov2007@yandex.ru>
2026-09-26 14:15:37 +0300

    fixed exec of wrong kernel

--- src/main_aplusb_matrix.cpp
+++ src/main_aplusb_matrix.cpp
@@ -125,7 +125,7 @@ void run(int argc, char** argv)
             // Если хотите - можете удалить ветвление здесь и оставить только тот код который соответствует вашему выбору API
             // TODO раскомментируйте вызов вашего API и поправьте его
             if (context.type() == gpu::Context::TypeOpenCL) {
-                ocl_aplusb_matrix_bad.exec(workSize, a_gpu, b_gpu, c_gpu, width, height);
+                ocl_aplusb_matrix_good.exec(workSize, a_gpu, b_gpu, c_gpu, width, height);
             } else if (context.type() == gpu::Context::TypeCUDA) {
                 // cuda::aplusb_matrix_bad(workSize, a_gpu, ...);
             } else if (context.type() == gpu::Context::TypeVulkan) {
